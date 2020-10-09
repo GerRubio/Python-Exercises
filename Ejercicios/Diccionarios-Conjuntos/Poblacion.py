5800e91e87bca3788f00a74d8a0b78d960f8894a
GerRubio <rubio.delavieja@gmail.com>
2020-10-09 22:33:39 +0200

    More lists exercises and new dictionaries exercises.

---
+++ Ejercicios/Diccionarios-Conjuntos/Poblacion.py
@@ -0,0 +1,19 @@
+cities_data = {'Tokyo': 38140000,
+    'Delhi': 26454000,
+    'Shanghai': 24484000,
+    'Mumbai': 21357000,
+    'São Paulo': 21297000
+}
+
+total_population = 0
+
+# Total population.
+for population in cities_data.values():
+    total_population += population
+
+# Relative population.
+for city in cities_data:
+    cities_data[city] = (cities_data[city] / total_population) * 100
+    
+print(f'Total population: {total_population}')
+print(cities_data)
